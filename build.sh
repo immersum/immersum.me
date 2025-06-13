914cae83d71be5f7f68ddab978d77bd43911a35e
immersum <immersum@protonmail.com>
2025-06-13 13:28:00 +0200

    build: Remove docker commands from build.sh and add package.json for css

--- build.sh
+++ build.sh
@@ -6,17 +6,5 @@ SOURCE_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 # create blog comments files, if needed
 "$SOURCE_DIR/retrieve-comments.py" --create-empty
 
-# create the container for zola builds and tailwindcss updates
-docker build -t develop-zola-tailwindcss "${SOURCE_DIR}/themes/project-portfolio" || exit 2
-
-# update the CSS file using tailwindcss
-docker run --rm -v ${SOURCE_DIR}:/source:Z -w /source develop-zola-tailwindcss \
-    tailwindcss -i src/css/main.css -o static/css/main.css --minify || exit 3
-
 # build the website via zola
-if command -v zola >/dev/null 2>&1; then
-    zola build
-else
-    docker run --rm -v ${SOURCE_DIR}:/source:Z -w /source develop-zola-tailwindcss \
-        zola build || exit 3
-fi
+npm install && npm run build && zola build
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "content/**/*.md",
+    "static/js/**/*.js",
+    "templates/**/*.html",
+    "themes/project-portfolio/static/js/**/*.js",
+    "themes/project-portfolio/templates/**/*.html",
+  ],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
